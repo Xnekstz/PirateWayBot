523302d6b2b15e8628c5cd82e5de3812fd20d688
Thalison <122047185+Xnekstz@users.noreply.github.com>
2025-03-15 15:57:52 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import discord
 import os
+import json
 from discord.ext import commands
 from bs4 import BeautifulSoup
 
-TOKEN = ''
-PREFIX = '?'
-HTML_FOLDER = 'html'
+with open("config.json", "r", encoding="utf-8") as config_file:
+    config = json.load(config_file)
+
+TOKEN = config['TOKEN']
+PREFIX = config['PREFIX']
+HTML_FOLDER = config['HTML_FOLDER']
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
@@ -17,13 +21,14 @@ async def on_ready():
         synced = await bot.tree.sync()
         print(f'📌 {len(synced)} slash commands synced.')
     except Exception as e:
-        print(f'Erro ao sincronizar: {e}')
+        print(f'Sync error: {e}')
     print('-----')
 
 # ---- FUNCTIONS ----
 def load_links():
     links_elamigos = []
     links_steamrip = []
+    links_dodi = []
 
     if not os.path.exists(HTML_FOLDER):
         os.makedirs(HTML_FOLDER)
@@ -41,36 +46,208 @@ def load_links():
                         link_url = f'https://steamrip.com{link_url}'
 
                     if link_url.startswith('https://elamigos.site'):
-                        links_elamigos.append(f"[{link_text}]({link_url})")
+                        links_elamigos.append(f'[{link_text}]({link_url})')
                     if link_url.startswith('https://steamrip.com'):
-                        links_steamrip.append(f"[{link_text}]({link_url})")
+                        links_steamrip.append(f'[{link_text}]({link_url})')
+                    if link_url.startswith('https://dodi-repacks.site'):
+                        links_dodi.append(f'[{link_text}]({link_url})')
 
                     
     
-    return links_elamigos, links_steamrip
+    return links_elamigos, links_steamrip, links_dodi
 
 # ---- SLASH COMMANDS ----
 @bot.tree.command(name='search', description='Offline database search for cracked games.')
 async def search(interaction: discord.Interaction, input: str):
     await interaction.response.defer(thinking=True)
 
-    links_elamigos, links_steamrip = load_links()
+    links_elamigos, links_steamrip, links_dodi = load_links()
     input = input.lower()
     results_elamigos = [link for link in links_elamigos if input in link.lower()]
     results_steamrip = [link for link in links_steamrip if input in link.lower()]
+    results_dodi = [link for link in links_dodi if input in link.lower()]
 
-    if not results_elamigos and not results_steamrip:
-        await interaction.response.send_message('Oops! I couldnt find any results :(')
+    if not results_elamigos and not results_steamrip and not results_dodi:
+        await interaction.followup.send('Oops! I couldnt find any results :(')
         return
 
-    embed = discord.Embed(title=f"🔎 Results for '{input}'", color=discord.Color.green())
+    # EMOJIS - Change to your own ids or just use regular emojis on field names
+    elamigos_emoji = config['ELAMIGOS_EMOJI']
+    steamrip_emoji = config['STEAMRIP_EMOJI']
+    dodi_emoji = config['DODI_EMOJI']
+
+    embed = discord.Embed(title=f"🔎 Results for '{input}'", color=discord.Color.dark_gray())
     if results_elamigos:
-        embed.add_field(name='🌐 ELAMIGOS', value='\n'.join(results_elamigos[:5]), inline=False)
+        embed.add_field(name=f'{elamigos_emoji} ELAMIGOS', value='\n'.join(results_elamigos[:5]), inline=False)
     if results_steamrip:
-        embed.add_field(name='🔥 STEAMRIP', value='\n'.join(results_steamrip[:5]), inline=False)
+        embed.add_field(name=f'{steamrip_emoji} STEAMRIP', value='\n'.join(results_steamrip[:5]), inline=False)
+    if results_dodi:
+        embed.add_field(name=f'{dodi_emoji} DODI REPACKS', value='\n'.join(results_dodi[:5]), inline=False)
 
 
     await interaction.followup.send(embed=embed)
 
+@bot.tree.command(name='games', description='Popular websites for cracked games.')
+async def games(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+
+    embed = discord.Embed(title='Cracked Games Websites', color=discord.Color.dark_gray())
+    embed.add_field( # DDL
+        name='Direct Download Links (DDL)',
+        value='- [SteamRIP](https://steamrip.com/)\n'
+        '- [ElAmigos](https://elamigos.site/)',
+        inline=False
+        )
+    embed.add_field( # Torrents
+        name='Torrents (Repacks)',
+        value='- [Fitgirl](https://fitgirl-repacks.site/popular-repacks/)\n'
+        '- [Dodi](https://dodi-repacks.site/)',
+        inline=False
+    )
+
+    await interaction.followup.send(embed=embed)
+
+@bot.tree.command(name='emulators', description='Popular emulators and roms websites.')
+async def emulators(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+
+    embed = discord.Embed(title='Emulators and Roms Popular Websites', color=discord.Color.dark_gray())
+    embed.add_field(
+        name='Multisystem',
+        value='- [OpenEmu](https://openemu.org/) - MacOS\n' \
+        '- [RetroArch](https://www.retroarch.com/) - Windows\n' \
+        '- [Retrodeck](https://retrodeck.net/) - Linux',
+        inline=False
+    )
+    embed.add_field(
+        name='Microsoft',
+        value='- [Xenia](https://xenia.jp/) - Xbox 360',
+        inline=False
+    )
+    embed.add_field(
+        name='Sony',
+        value='- [DuckStation](https://www.duckstation.org/) - PS1\n'
+        '- [PCSX2](https://pcsx2.net/) - PS2\n'
+        '- [RPCS3](https://rpcs3.net/) - PS3\n'
+        '- [PPSSPP](https://www.ppsspp.org/) - PSP\n'
+        '- [VITA3K](https://vita3k.org/) - PSVITA',
+        inline=False
+    )
+    embed.add_field(
+        name='Nintendo',
+        value='- [SNES9X](https://www.snes9x.com/) - SNES\n'
+        '- [MGba](https://mgba.io/) - Game Boy Advance\n'
+        '- [Suyu](https://suyu.dev/) - Switch\n'
+        '- [Torzu](https://torzu.dev/) - Switch',
+        inline=False
+    )
+    embed.add_field(
+        name='Roms',
+        value='- [GameGinie](https://www.gameginie.com/)\n'
+        '- [ROMSPURE](https://romspure.cc/) - Over 30,000 verified roms.\n'
+        '- [RomHacking](https://www.romhacking.net/) - Hub for roms modifications.\n'
+        '- [ConsoleRoms](https://www.consoleroms.com/) - Console roms and emulators.\n'
+        '- [Vimms Lair](https://vimm.net/)',
+        inline=False
+    )
+
+    await interaction.followup.send(embed=embed)
+
+@bot.tree.command(name='softwares', description='A selection of good websites for downloading cracked softwares.')
+async def softwares(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+
+    embed = discord.Embed(
+        title='Cracked Softwares',
+        description='- [AudioZ](https://audioz.download/) - Music production softwares\n'
+        '- [Cracksurl](https://cracksurl.com/) - "The ultimate software hub"\n'
+        '- [Monkrus](https://w16.monkrus.ws/) - All Adobe softwares',
+        color=discord.Color.dark_gray()
+        )
+    
+    await interaction.followup.send(embed=embed)
+
+@bot.tree.command(name='books', description='Popular websites for downloading and reading books.')
+async def books(interaction: discord.Interaction):
+    await interaction.response.defer()
+
+    embed = discord.Embed(title='Free Books for Download', color=discord.Color.dark_gray())
+    embed.add_field(
+        name='Browser Reading',
+        value='- [Read Comic Online](https://readcomiconline.li/) - Free and online comics.\n'
+        '- [Novel Buddy](https://novelbuddy.com/official) - Light novels.',
+        inline=False
+    )
+    embed.add_field(
+        name='Direct Download Links (DDL)',
+        value='- [Z-Lib](https://z-library.sk/) - Most popular online library.\n'
+        '- [Z-Lib #2](https://z-lib.io/)\n'
+        '- [Annas Archive](https://Annas-archive.org)\n'
+        '- [Archive Organization](https://archive.org/)\n'
+        '- [LibGen](https://libgen.mx)\n'
+        '- [PdfDrive](https://pdfdrive.com)\n'
+        '- [GetComics](https://getcomics.org/)',
+        inline=False
+    )
+    embed.add_field(
+        name='Manga',
+        value='- [MangaBuddy](https://mangabuddy.com/) - Huge database.\n'
+        '- [MangaFire](https://mangafire.to/)',
+        inline=False
+    )
+    embed.add_field(
+        name='IMPORTANT NOTE',
+        value='Try also searching for `<bookname> PDF`'
+    )
+
+    await interaction.followup.send(embed=embed)
+
+@bot.tree.command(name='media', description='Websites for watching online movies for free.')
+async def media(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+
+    embed = discord.Embed(title='Free Media', color=discord.Color.dark_gray())
+    embed.add_field(
+        name='Online',
+        value='- [Hura Watch](https://hurawatch.cc/home)\n'
+        '- [Alphatron](https://alphatron.tv/)\n'
+        '- [123Netmovies](https://123netmovies.com/)',
+        inline=False
+    )
+    embed.add_field(
+        name='Torrent or DDL',
+        value='- [Ashutosh Git Page](https://github.com/Ashutoshwahane/TorrentFiles/)\n'
+        '- [FMHY](https://fmhy.pages.dev/torrentpiracyguide#torrent-sites)\n'
+        '- [1337x](https://www.1377x.to/popular-movies) - Best one.',
+        inline=False
+    )
+
+    await interaction.followup.send(embed=embed)
+
+@bot.tree.command(name='vpns', description='Unordened list of the best VPNs.')
+async def vpns(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+
+    embed = discord.Embed(
+        title='VPNs',
+        description='- [ProtonVPN](https://protonvpn.com/) - Free version cant torrent.\n'
+        '- [Mullvad](https://mullvad.net/en) - Paid only.\n'
+        '- [Windscribe](https://windscribe.com/) - Free and P2P.',
+        color=discord.Color.dark_gray()
+        )
+
+    await interaction.followup.send(embed=embed)
+
+@bot.tree.command(name='all_purpose', description='All purpose search tools.')
+async def all_purpose(interaction: discord.Interaction):
+    await interaction.response.defer()
+
+    embed = discord.Embed(
+        title='All Purpose Search Tools',
+        description='- [RuTracker](https://rutracker.org/) - My favorite.\n'
+        '- [1337x](https://www1.13377x.tw/) - A good old one.'
+    )
+
+    await interaction.followup.send(embed=embed)
 
 bot.run(TOKEN)
